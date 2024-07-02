150324525beb13f3e70440c47b753e7aac121e7d
minaripenguin <minaripenguin@users.noreply.github.com>
2024-07-03 07:49:20 +0800

    Generate extra vendor for prebuilt vendors (cheetah)
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

---
+++ setup-makefiles.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+DEVICE=cheetah
+VENDOR=google
+
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
+
+export ANDROID_ROOT=$(pwd)
+
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
+    exit 1
+fi
+source "${HELPER}"
+
+# Initialize the helper
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" true
+
+# Warning headers and guards
+write_headers "arm64"
+sed -i 's|TARGET_DEVICE|TARGET_ARCH|g' "${ANDROIDMK}"
+sed -i 's|vendor/google/|vendor/google/cheetah|g' "${PRODUCTMK}"
+sed -i 's|device/google//setup-makefiles.sh|vendor/google/setup-makefiles.sh|g' "${ANDROIDBP}" "${ANDROIDMK}" "${BOARDMK}" "${PRODUCTMK}"
+
+write_makefiles "${MY_DIR}/proprietary-files.txt" true
+
+# Finish
+write_footers
